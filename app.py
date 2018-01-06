e80f9f41757d4333132452cc8f567a78d5504a38
yudaishimanaka <gurutaminn.h8@gmail.com>
2018-01-07 03:13:46 +0900

    completed register

--- app.py
+++ app.py
@@ -1,10 +1,50 @@
 from flask import *
 from views import views
+from database import Session as Ss
+from models import *
+from passlib.hash import pbkdf2_sha256
+import re
 
 
 app = Flask(__name__)
 
 app.register_blueprint(views.views)
+
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    if request.method == 'POST':
+        user_name = request.json['user_name']
+        email = request.json['email']
+        password = pbkdf2_sha256.hash(request.json['password'])
+
+        if user_name and email and password:
+            email_pattern = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+            email_re_pattern = re.compile(email_pattern)
+
+            if email_re_pattern.match(email):
+                result = Ss.query(User).filter_by(email=email)
+
+                if result.count() > 0:
+                    status = jsonify([0, "this email address is already in use"])
+                else:
+                    user = User(
+                        user_name=user_name,
+                        email=email,
+                        password=password
+                    )
+                    Ss.add(user)
+                    Ss.commit()
+                    Ss.close()
+                    status = jsonify([1, "register successfully"])
+
+            else:
+                status = jsonify([2, "wrong email format"])
+
+        else:
+            status = jsonify([3, "don't completed input"])
+
+        return status
 
 
 if __name__ == '__main__':
---
+++ static/js/register.js
@@ -0,0 +1,29 @@
+$(function(){
+    $("#register").click(function(){
+        $.ajax({
+            type: 'POST',
+            url: '/create_user',
+            data: JSON.stringify({"user_name":$("#user_name").val(),
+                                  "email":$("#email").val(),
+                                  "password":$("#password").val()
+                                }),
+            contentType: 'application/json',
+            success: function(response){
+                console.log(response)
+                var color;
+                if(response[0] == 1){
+                    color = "green";
+                    $("#user_name").val("");
+                    $("#email").val("");
+                    $("#password").val("");
+                }else{
+                    color = "red";
+                }
+                Materialize.toast(response[1], 5000, color + ' rounded')
+            },
+            error: function(response){
+                console.log("application error")
+            }
+        })
+    })
+})
