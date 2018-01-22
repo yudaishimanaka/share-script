380d922026f157ea45fd05033368cf452dde9728
yudaishimanaka <gurutaminn.h8@gmail.com>
2018-01-23 03:08:29 +0900

    fixed setting.html does return user object

--- views/views.py
+++ views/views.py
@@ -10,9 +10,11 @@ views = Blueprint('views', __name__)
 def register():
     return render_template('register.html')
 
+
 @views.route('/login', methods=['GET'])
 def login():
     return render_template('login.html')
+
 
 @views.route('/index', methods=['GET'])
 def index():
@@ -22,6 +24,11 @@ def index():
     else:
         return render_template('index.html')
 
+
 @views.route('/setting', methods=['GET'])
 def setting():
-    return render_template('setting.html')
+    if session:
+        user = Ss.query(User).filter_by(user_id=session['user_id']).one()
+        return render_template('setting.html', user=user)
+    else:
+        return render_template('setting.html')
